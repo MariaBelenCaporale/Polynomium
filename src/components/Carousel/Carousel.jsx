c1a7dbd6e7e5c591704c78f5addd282791437cfe
Belen <belen.c@polynomium.com>
2024-10-03 17:16:39 -0300

    btn carousel

--- src/components/Carousel/Carousel.jsx
+++ src/components/Carousel/Carousel.jsx
@@ -1,6 +1,7 @@
+import  { useRef } from 'react';
 import "./styles.css";
-// import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded';
-// import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
+import KeyboardArrowLeftRoundedIcon from '@mui/icons-material/KeyboardArrowLeftRounded';
+import KeyboardArrowRightRoundedIcon from '@mui/icons-material/KeyboardArrowRightRounded';
 import CardCarousel from "../CardCarousel/CardCarousel";
 import Moneda from '../../images/coin.png';
 import Ruleta from '../../images/ruleta.png';
@@ -8,79 +9,96 @@ import Fluence from '../../images/fluence.png';
 
 import "swiper/swiper-bundle.css"; 
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Navigation, Pagination, EffectCoverflow } from "swiper/modules";
+import { Pagination, EffectCoverflow } from "swiper/modules";
 
 const Carousel = () => {
+  const swiperRef = useRef(null);
+
+  const handlePrev = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slidePrev();
+    }
+  };
+
+  const handleNext = () => {
+    if (swiperRef.current) {
+      swiperRef.current.swiper.slideNext();
+    }
+  };
+
   return (
     <section className="sectionCarousel">
       <div className="contenedorCarouselTotal">
-      {/* <button className='btnCarousel'>
-                <KeyboardArrowLeftRoundedIcon />
-            </button> */}
-      <div className="fraseFondo">
-        <h2 className="fraseFondoTexto">Nuestros productos</h2>
-      </div>
- 
-      <div className="containerLuzUno">
-        <div className="luzUno"></div>
-        <div className="luzUnoA"></div>
-      </div>
+        <div className="fraseFondo">
+          <h2 className="fraseFondoTexto">Nuestros productos</h2>
+        </div>
 
-      <Swiper
-        modules={[Navigation, Pagination, EffectCoverflow]}
-        navigation
-        pagination={{ clickable: true }}
-        loop={true}
-        effect="coverflow"
-        grabCursor={true}
-        centeredSlides={true}
-        spaceBetween={300}
-        coverflowEffect={{
-          rotate: 0,
-          stretch: 10,
-          depth: 900,
-          modifier: 1,
-          slideShadows: false,
-        }}
-        slidesPerView={1}
-      >
-        <SwiperSlide>
-          <CardCarousel
-            miniTitulo="Proyectos"
-            titulo="Megapix"
-            textoUno="Revolucionando los Pagos con PIX"
-            texto={
-              "Forjamos un ambiente donde los comerciantes prosperan y los turistas disfrutan de pagos sin complicaciones, derribando barreras y construyendo conexiones sin fronteras."
-            }
-            imageCarousel={Moneda}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCarousel
-            miniTitulo="Proyectos"
-            titulo="Assiria"
-            textoUno="Plataformas Igaming"
-            texto={
-              "Creamos universos virtuales donde el igaming no es solo entretenimiento, sino una experiencia inmersiva y transformadora."
-            }
-            imageCarousel={Ruleta}
-          />
-        </SwiperSlide>
-        <SwiperSlide>
-          <CardCarousel 
-               miniTitulo='Proyectos'
-               titulo='Fluence'
-               textoUno={'Sigue al conejo'}
-               texto={'Ser parte de Fluverse implica más que unirse a una plataforma: es adentrarse en un universo de educación financiera.'}
-               imageCarousel={Fluence}
-          />
-        </SwiperSlide>
-      </Swiper>
+        <div className="containerLuzUno">
+          <div className="luzUno"></div>
+          <div className="luzUnoA"></div>
+        </div>
+        <div className='containerBtnCarousel'>
+        <button className='btnCarousel' onClick={handlePrev}>
+          <KeyboardArrowLeftRoundedIcon />
+        </button>
+        </div>
+        <Swiper
+          ref={swiperRef}
+          modules={[Pagination, EffectCoverflow]}
+          pagination={{ clickable: true }}
+          loop={true}
+          effect="coverflow"
+          grabCursor={true}
+          centeredSlides={true}
+          spaceBetween={600}
+          coverflowEffect={{
+            rotate: 0,
+            stretch: 10,
+            depth: 900,
+            modifier: 1,
+            slideShadows: false,
+          }}
+          slidesPerView={1}
+        >
+          <SwiperSlide>
+            <CardCarousel
+              miniTitulo="Proyectos"
+              titulo="Megapix"
+              textoUno="Revolucionando los Pagos con PIX"
+              texto={
+                "Forjamos un ambiente donde los comerciantes prosperan y los turistas disfrutan de pagos sin complicaciones, derribando barreras y construyendo conexiones sin fronteras."
+              }
+              imageCarousel={Moneda}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCarousel
+              miniTitulo="Proyectos"
+              titulo="Assiria"
+              textoUno="Plataformas Igaming"
+              texto={
+                "Creamos universos virtuales donde el igaming no es solo entretenimiento, sino una experiencia inmersiva y transformadora."
+              }
+              imageCarousel={Ruleta}
+            />
+          </SwiperSlide>
+          <SwiperSlide>
+            <CardCarousel 
+              miniTitulo='Proyectos'
+              titulo='Fluence'
+              textoUno={'Sigue al conejo'}
+              texto={'Ser parte de Fluverse implica más que unirse a una plataforma: es adentrarse en un universo de educación financiera.'}
+              imageCarousel={Fluence}
+            />
+          </SwiperSlide>
+        </Swiper>
 
-      {/* <button className='btnCarousel'>
-                <KeyboardArrowRightRoundedIcon />
-            </button> */}
-            </div>
+        <div className='containerBtnCarousel'>
+        <button className='btnCarousel' onClick={handleNext}>
+          <KeyboardArrowRightRoundedIcon />
+        </button>
+        </div>
+      </div>
     </section>
   );
 };
